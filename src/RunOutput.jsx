d3a9213af69fd87c731afffd5263c7a0d0410a24
Daniel Nolan <DanNolan99@gmail.com>
2018-05-10 10:44:49 -0400

    adds web pack for building

--- src/RunOutput.jsx
+++ src/RunOutput.jsx
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import RunResults from './RunResults';
 import RunErrors from './RunErrors';
-import './RunOutput.css'
 
 class RunOutput extends Component {
     render() {

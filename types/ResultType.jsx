b65c7b287f9140b2b7f7941cbb0da3149411f99d
Daniel Nolan <DanNolan99@gmail.com>
2018-05-11 17:52:40 -0400

    adds inline SVGs

--- types/ResultType.jsx
+++ types/ResultType.jsx
@@ -1,11 +1,20 @@
 import React, {Component} from 'react';
+import Check from './Check';
+import Fail from './Fail';
 
 class ResultType extends Component {
+    renderSVG() {
+        if(this.props.item.t === 'passed') {
+            return <Check />
+        }
+        return <Fail />
+    }
     render() {
         const {item} = this.props;
         const value = item.v.replace(/^[\n ]*/, "").trim();
         return (
             <div className={`result-type result-type--${item.t} ${item.t}`}>
+                { this.renderSVG() }
                 <span className={`result-type__value ${item.t}`}>{ value }</span>
             </div>
 
